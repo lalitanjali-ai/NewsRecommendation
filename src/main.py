19f09ea8bf6e60208aa0ae2425173379ba66c911
lalitanjali-ai <lab164@g.harvard.edu>
2023-05-02 23:59:06 -0400

    updated test function and added loss plots

--- src/main.py
+++ src/main.py
@@ -24,6 +24,9 @@ from preprocess import read_news, get_doc_input
 from metrics import roc_auc_score, ndcg_score, mrr_score
 from torch.nn.utils.rnn import pad_sequence
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 nltk.download('averaged_perceptron_tagger')
@@ -41,7 +44,8 @@ def train(rank, args):
         utils.setuplogger()
         dist.init_process_group('nccl', world_size=args.nGPU, init_method='env://', rank=rank)
 
-    torch.cuda.set_device(rank)
+    if args.enable_gpu:
+        torch.cuda.set_device(rank)
 
     if args.use_topics:
         bert_topics_train = pd.read_csv("bert_topics/train_small_bert_topics.tsv", sep='\t')
@@ -110,6 +114,8 @@ def train(rank, args):
     dataloader = DataLoader(dataset, batch_size=args.batch_size, drop_last=True)
 
     logging.info('Training...')
+    train_losses = []
+
     for ep in range(args.start_epoch, args.epochs):
         loss = 0.0
         accuary = 0.0
@@ -130,27 +136,7 @@ def train(rank, args):
             bz_loss.backward()
             optimizer.step()
 
-            if cnt % args.log_steps == 0:
-                logging.info(
-                    '[{}] Ed: {}, train_loss: {:.5f}, acc: {:.5f}'.format(
-                        rank, cnt * args.batch_size, loss.data / cnt, accuary / cnt)
-                )
-
-            if rank == 0 and cnt != 0 and cnt % args.save_steps == 0:
-                ckpt_path = os.path.join(args.model_dir, f'epoch-{ep + 1}-{cnt}.pt')
-                torch.save(
-                    {
-                        'model_state_dict':
-                            {'.'.join(k.split('.')[1:]): v for k, v in model.state_dict().items()}
-                            if is_distributed else model.state_dict(),
-                        'category_dict': category_dict,
-                        'word_dict': word_dict,
-                        'subcategory_dict': subcategory_dict,
-                        'abs_word_dict': abs_word_dict,
-                        'embedding_matrix': embedding_matrix,
-
-                    }, ckpt_path)
-                logging.info(f"Model saved to {ckpt_path}.")
+        train_losses.append(loss.data / cnt) # Append the loss for the current epoch
 
         logging.info('Training finish.')
 
@@ -165,11 +151,12 @@ def train(rank, args):
                     'subcategory_dict': subcategory_dict,
                     'word_dict': word_dict,
                     'abs_word_dict': abs_word_dict,
-                    'embedding_matrix': embedding_matrix,
+                    # 'embedding_matrix': embedding_matrix,
 
                 }, ckpt_path)
             logging.info(f"Model saved to {ckpt_path}.")
 
+    return train_losses
 
 def test(rank, args):
     if rank is None:
@@ -182,12 +169,20 @@ def test(rank, args):
         utils.setuplogger()
         dist.init_process_group('nccl', world_size=args.nGPU, init_method='env://', rank=rank)
 
-    torch.cuda.set_device(rank)
 
     if args.use_topics:
         bert_topics_test = pd.read_csv("bert_topics/val_small_bert_topics.tsv", sep='\t')
 
-    news, news_index, category_dict, subcategory_dict, word_dict, abs_word_dict = read_news(
+    # Load model from checkpoint
+    ckpt_path = utils.get_checkpoint(args.model_dir, args.load_ckpt_name)
+    checkpoint = torch.load(ckpt_path, map_location='cpu')
+
+    subcategory_dict = checkpoint['subcategory_dict']
+    category_dict = checkpoint['category_dict']
+    word_dict = checkpoint['word_dict']
+    abs_word_dict = checkpoint['abs_word_dict']
+
+    news, news_index  = read_news(
         os.path.join(args.test_data_dir, 'news.tsv'), args, mode='test', bert_topics_train=bert_topics_test)
 
     news_title, news_category, news_subcategory, news_abstract = get_doc_input(
@@ -205,13 +200,13 @@ def test(rank, args):
     else:
         news_combined = np.concatenate([x for x in [news_title] if x is not None], axis=-1)
 
-    # Load model from checkpoint
-    ckpt_path = utils.get_checkpoint(args.model_dir, args.load_ckpt_name)
-    checkpoint = torch.load(ckpt_path, map_location='cpu')
 
-    embedding_matrix = checkpoint['embedding_matrix']
+
+    # embedding_matrix = checkpoint['embedding_matrix']
+    dummy_embedding_matrix = np.zeros((len(word_dict) + 1, args.word_embedding_dim))
+
     module = importlib.import_module(f'model.{args.model}')
-    model = module.Model(args, embedding_matrix, len(category_dict), len(subcategory_dict))
+    model = module.Model(args, dummy_embedding_matrix, len(category_dict), len(subcategory_dict))
     model.load_state_dict(checkpoint['model_state_dict'])
 
     if args.enable_gpu:
@@ -220,82 +215,138 @@ def test(rank, args):
     if is_distributed:
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank])
 
+
     model.eval()
 
+    news_dataset = NewsDataset(news_combined)
+    news_dataloader = DataLoader(news_dataset,
+                                 batch_size=args.batch_size,
+                                 num_workers=4)
 
-    def custom_collate_fn(batch):
-        user_titles, user_abstracts, log_mask, news_titles, news_abstracts, targets = zip(*batch)
+    news_scoring = []
+    with torch.no_grad():
+        for input_ids in tqdm(news_dataloader):
+            if args.enable_gpu:
+                input_ids = input_ids.cuda(rank)
+            b_size, d_size = input_ids.shape
+            news_vec = model.news_encoder(input_ids[:b_size, :d_size//2], input_ids[:b_size, d_size//2:])
+            news_vec = news_vec.to(torch.device("cpu")).detach().numpy()
+            news_scoring.extend(news_vec)
 
-        user_titles = pad_sequence([torch.tensor(t) for t in user_titles], batch_first=True, padding_value=0)
-        user_abstracts = pad_sequence([torch.tensor(a) for a in user_abstracts], batch_first=True, padding_value=0)
-        log_mask = torch.stack(
-            [torch.tensor(mask) for mask in log_mask])  # Convert log_mask to a Tensor before stacking
-        news_titles = pad_sequence([torch.tensor(t) for t in news_titles], batch_first=True, padding_value=0)
-        news_abstracts = pad_sequence([torch.tensor(a) for a in news_abstracts], batch_first=True, padding_value=0)
+    news_scoring = np.array(news_scoring)
 
-        # Pad the targets to have the same length
-        targets = pad_sequence([torch.tensor(target) for target in targets], batch_first=True, padding_value=0)
 
-        logging.info("user_titles.shape", user_titles.shape)
-        logging.info("user_abstracts.shape", user_abstracts.shape)
-        logging.info("log_mask.shape", log_mask.shape)
-        logging.info("news_titles.shape", news_titles.shape)
-        logging.info("news_abstracts.shape", news_abstracts.shape)
-        logging.info("targets.shape", targets.shape)
-
-        return user_titles, user_abstracts, log_mask, news_titles, news_abstracts, targets
+    def collate_fn(tuple_list):
+        log_vecs = torch.FloatTensor([x[0] for x in tuple_list])
+        log_mask = torch.FloatTensor([x[1] for x in tuple_list])
+        news_vecs = [x[2] for x in tuple_list]
+        labels = [x[3] for x in tuple_list]
+        return (log_vecs, log_mask, news_vecs, labels)
 
     data_file_path = os.path.join(args.test_data_dir, f"behaviors_{rank}.tsv")
 
-    dataset = DatasetTest(data_file_path, news_index, news_combined, args)
-    dataloader = DataLoader(dataset, batch_size=args.batch_size, drop_last=True, collate_fn=custom_collate_fn)
-
-    logging.info('Testing...')
-    loss = 0.0
-    accuracy = 0.0
-    cnt = 0
+    dataset = DatasetTest(data_file_path, news_index, news_scoring, args)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, drop_last=True, collate_fn=collate_fn)
+    from metrics import roc_auc_score, ndcg_score, mrr_score
 
     AUC = []
     MRR = []
     nDCG5 = []
     nDCG10 = []
+    test_metrics = []
 
-    with torch.no_grad():
-        for user_titles, user_abstracts, log_mask, news_titles, news_abstracts, targets in dataloader:
-            if args.enable_gpu:
-                user_titles = user_titles.cuda(rank, non_blocking=True)
-                user_abstracts = user_abstracts.cuda(rank, non_blocking=True)
-                log_mask = log_mask.cuda(rank, non_blocking=True)
-                news_titles = news_titles.cuda(rank, non_blocking=True)
-                news_abstracts = news_abstracts.cuda(rank, non_blocking=True)
-                targets = targets.cuda(rank, non_blocking=True)
+    def print_metrics(rank, cnt, x):
+        logging.info("[{}] {} samples: {}".format(rank, cnt, '\t'.join(["{:0.2f}".format(i * 100) for i in x])))
 
-            bz_loss, y_hat = model(user_titles, user_abstracts, log_mask, news_titles, news_abstracts)
-            loss += bz_loss.data.float()
-            accuracy += utils.acc(targets, y_hat)
-            cnt += 1
+    def get_mean(arr):
+        return [np.array(i).mean() for i in arr]
 
-        # Metric calculations
-        auc = roc_auc_score(targets.cpu().numpy(), y_hat.cpu().numpy())
-        mrr = mrr_score(targets.cpu().numpy(), y_hat.cpu().numpy())
-        ndcg5 = ndcg_score(targets.cpu().numpy(), y_hat.cpu().numpy(), k=5)
-        ndcg10 = ndcg_score(targets.cpu().numpy(), y_hat.cpu().numpy(), k=10)
+    def get_sum(arr):
+        return [np.array(i).sum() for i in arr]
 
-        AUC.append(auc)
-        MRR.append(mrr)
-        nDCG5.append(ndcg5)
-        nDCG10.append(ndcg10)
+    local_sample_num = 0
 
-    # Log final results
-    logging.info(f"Mean AUC: {np.mean(AUC)}")
-    logging.info(f"Mean MRR: {np.mean(MRR)}")
-    logging.info(f"Mean nDCG@5: {np.mean(nDCG5)}")
-    logging.info(f"Mean nDCG@10: {np.mean(nDCG10)}")
+    for cnt, (log_vecs, log_mask, news_vecs, labels) in enumerate(dataloader):
+        local_sample_num += log_vecs.shape[0]
 
-    logging.info(f'Test_loss: {loss.data / cnt}, Test_acc: {accuracy / cnt}')
+        if args.enable_gpu:
+            log_vecs = log_vecs.cuda(rank, non_blocking=True)
+            log_mask = log_mask.cuda(rank, non_blocking=True)
 
+        user_vecs = model.user_encoder(log_vecs, log_mask).to(torch.device("cpu")).detach().numpy()
+
+        for user_vec, news_vec, label in zip(user_vecs, news_vecs, labels):
+            if label.mean() == 0 or label.mean() == 1:
+                continue
+
+            score = np.dot(news_vec, user_vec)
+
+            auc = roc_auc_score(label, score)
+            mrr = mrr_score(label, score)
+            ndcg5 = ndcg_score(label, score, k=5)
+            ndcg10 = ndcg_score(label, score, k=10)
+
+            AUC.append(auc)
+            MRR.append(mrr)
+            nDCG5.append(ndcg5)
+            nDCG10.append(ndcg10)
+            test_metrics.append([auc, mrr, ndcg5, ndcg10])
+
+        if cnt % args.log_steps == 0:
+            print_metrics(rank, local_sample_num, get_mean([AUC, MRR, nDCG5, nDCG10]))
+
+    logging.info('[{}] local_sample_num: {}'.format(rank, local_sample_num))
+    if is_distributed:
+        local_sample_num = torch.tensor(local_sample_num).cuda(rank)
+        dist.reduce(local_sample_num, dst=0, op=dist.ReduceOp.SUM)
+        local_metrics_sum = torch.FloatTensor(get_sum([AUC, MRR, nDCG5, nDCG10])).cuda(rank)
+        dist.reduce(local_metrics_sum, dst=0, op=dist.ReduceOp.SUM)
+        if rank == 0:
+            print_metrics('*', local_sample_num, local_metrics_sum / local_sample_num)
+    else:
+        print_metrics('*', local_sample_num, get_mean([AUC, MRR, nDCG5, nDCG10]))
+
+    return test_metrics
+
+def plot_train_loss_curves(train_losses):
+    plt.figure()
+    plt.plot(train_losses, label='Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('train_loss_curve.png')
+
+def plot_test_loss_curves(train_losses):
+
+    plt.figure()
+    test_metrics_mean = np.mean(test_metrics, axis=0)
+    plt.plot(test_metrics_mean[:, 0], label='AUC')
+    plt.plot(test_metrics_mean[:, 1], label='MRR')
+    plt.plot(test_metrics_mean[:, 2], label='nDCG5')
+    plt.plot(test_metrics_mean[:, 3], label='nDCG10')
+    plt.xlabel('Batch')
+    plt.ylabel('Metrics')
+    plt.legend()
+    plt.savefig('test_metrics_curve.png')
 
 if __name__ == "__main__":
+    # utils.setuplogger()
+    # args = parse_args()
+    # utils.dump_args(args)
+    # random.seed(args.seed)
+    # torch.manual_seed(args.seed)
+    # os.environ['MASTER_ADDR'] = 'localhost'
+    # os.environ['MASTER_PORT'] = '8888'
+    # Path(args.model_dir).mkdir(parents=True, exist_ok=True)
+    # args.model_dir = '../model'
+    # args.model = 'NRMS_abstract'
+    # args.mode = 'test'
+    # args.load_ckpt_name = 'epoch-1.pt'
+    # args.enable_gpu = False
+    # args.use_category = False
+    # args.use_subcategory = False
+    # args.prepare = True
+
     utils.setuplogger()
     args = parse_args()
     utils.dump_args(args)
@@ -327,7 +378,8 @@ if __name__ == "__main__":
             f'{total_sample_num} training samples, {total_sample_num // args.batch_size // args.nGPU} batches in total.')
 
         if args.nGPU == 1:
-            train(None, args)
+            train_losses = train(None, args)
+            plot_train_loss_curves(train_losses)
         else:
             torch.multiprocessing.spawn(train, nprocs=args.nGPU, args=(args,))
 
@@ -349,7 +401,7 @@ if __name__ == "__main__":
         logging.info(f'{total_sample_num} testing samples in total.')
 
         if args.nGPU == 1:
-            test(None, args)
+            test_metrics = test(None, args)
+            plot_test_loss_curves(train_losses)
         else:
             torch.multiprocessing.spawn(test, nprocs=args.nGPU, args=(args,))
-
